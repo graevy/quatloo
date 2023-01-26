577a47fac9546a282f0cc41205988b124225abfe
avery <avrypromo@pm.me>
2023-01-26 00:07:19 -0800

    cleaning up

--- resume_build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pandoc -s -t html -V fontsize=16 -V mainfont="Liberation Mono" -V backgroundcolor="#121215" -V fontcolor="#60b020" -o public/resume/index.html public/resume/resume.docx 2>/dev/null
