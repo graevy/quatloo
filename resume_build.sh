3cbec800af3245bf6703e40b59dc7142b06796b8
avery <avrypromo@pm.me>
2023-01-25 23:13:17 -0800

    resume tooling and color adjusting

--- resume_build.sh
+++ resume_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc -s -t html -V mainfont="Liberation Mono" -V backgroundcolor="#121215" -V fontcolor="#50b020" -o public/resume/index.html public/resume/resume.docx 2>/dev/null
+pandoc -s -t html -V fontsize=16 -V mainfont="Liberation Mono" -V backgroundcolor="#121215" -V fontcolor="#60b020" -o public/resume/index.html public/resume/resume.docx 2>/dev/null
